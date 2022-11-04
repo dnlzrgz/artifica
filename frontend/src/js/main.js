045fe5f3502fb1e51de3fe8535edbc93780d1b34
daniarlert <arlertdaniel@gmail.com>
2022-11-04 19:05:34 +0100

    feat(frontend): add sm size styles

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,15 +1,11 @@
 import interact from "interactjs";
 import "flowbite";
 
-interact(".applet").draggable({
-  inertia: true,
-  autoScroll: false,
-  listeners: {
-    move: dragMoveListener,
-  },
-});
+const applets = document.querySelectorAll(".applet");
+const mdSize = 768;
 
-function dragMoveListener(event) {
+// interact.js
+const dragMoveListener = (event) => {
   let target = event.target;
 
   // keep the dragged position in the data-x/data-y attributes
@@ -17,9 +13,53 @@ function dragMoveListener(event) {
   let y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
 
   // translate the element
-  target.style.transform = "translate(" + x + "px, " + y + "px)";
+  target.style.transform = `translate(${x}px, ${y}px)`;
 
   // update the posiion attributes
   target.setAttribute("data-x", x);
   target.setAttribute("data-y", y);
-}
+};
+
+interact(".applet").draggable({
+  enabled: window.innerWidth > mdSize,
+  inertia: true,
+  autoScroll: false,
+  listeners: {
+    move: dragMoveListener,
+  },
+});
+
+// z-index.
+let zIndex = 1;
+
+applets.forEach((applet) => {
+  applet.addEventListener("mousedown", () => {
+    zIndex++;
+    applet.style.zIndex = zIndex;
+  });
+});
+
+// move applets to random position.
+window.addEventListener("load", () => {
+  if (window.innerWidth < mdSize) {
+    return;
+  }
+
+  const windowWidth = window.innerWidth;
+  const widnowHeight = window.innerHeight;
+
+  applets.forEach((applet) => {
+    const appletWidth = applet.offsetWidth;
+    const appletHeight = applet.offsetHeight;
+
+    let x = Math.random() * (windowWidth - appletWidth);
+    let y = Math.random() * (widnowHeight - appletHeight);
+
+    // translate the element
+    applet.style.transform = `translate(${x}px, ${y}px)`;
+
+    // update the posiion attributes
+    applet.setAttribute("data-x", x);
+    applet.setAttribute("data-y", y);
+  });
+});
