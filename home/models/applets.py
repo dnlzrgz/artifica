e27340dcd0a01dee5215b2744fa7a40b58c946d5
daniarlert <arlertdaniel@gmail.com>
2022-11-05 11:26:52 +0100

    feat(home): add pomodoro, testimonies and timeline applet models

--- home/models/applets.py
+++ home/models/applets.py
@@ -1,5 +1,6 @@
 from wagtail.blocks import (
     CharBlock,
+    DateBlock,
     EmailBlock,
     ListBlock,
     RichTextBlock,
@@ -8,100 +9,6 @@ from wagtail.blocks import (
     URLBlock,
 )
 from wagtail.documents.blocks import DocumentChooserBlock
-
-
-class TextualApplet(StructBlock):
-    """
-    Applet with a rich text body.
-    """
-
-    class Meta:
-        template = "home/applets/textual_applet.html"
-
-    title = CharBlock(max_length=20)
-    content = RichTextBlock(
-        features=[
-            "h2",
-            "bold",
-            "italic",
-            "link",
-        ]
-    )
-
-
-class VideoApplet(StructBlock):
-    """
-    Applet to showcase videos.
-    """
-
-    class Meta:
-        template = "home/applets/video_applet.html"
-
-    title = CharBlock(max_length=20)
-    media = DocumentChooserBlock()
-
-
-class SocialLinksApplet(StructBlock):
-    """
-    Applet to display a list of links for social media like Instagram
-    or Twitter.
-    """
-
-    class Meta:
-        template = "home/applets/social_links_applet.html"
-
-    title = CharBlock(max_length=20)
-    links = ListBlock(
-        StructBlock(
-            [
-                ("title", CharBlock()),
-                ("url", URLBlock()),
-            ],
-            label="links",
-        )
-    )
-
-
-class NotesApplet(StructBlock):
-    """
-    Applet for F&Q.
-    """
-
-    class Meta:
-        template = "home/applets/notes_applet.html"
-
-    title = CharBlock(max_length=20)
-    notes = ListBlock(
-        StructBlock(
-            [
-                ("title", CharBlock()),
-                (
-                    "content",
-                    RichTextBlock(
-                        features=[
-                            "h2",
-                            "bold",
-                            "italic",
-                            "link",
-                        ]
-                    ),
-                ),
-            ],
-            label="notes",
-        )
-    )
-
-
-class TodoApplet(StructBlock):
-    """
-    Applet to display a functional ToDo list.
-    """
-
-    class Meta:
-        template = "home/applets/todo_applet.html"
-
-    title = CharBlock(max_length=20)
-    tasks = ListBlock(CharBlock(), label="tasks")
 
 
 class ButtonApplet(StructBlock):
@@ -129,15 +36,167 @@ class ContactApplet(StructBlock):
     email = EmailBlock()
 
 
+class NotesApplet(StructBlock):
+    """
+    Applet for F&Q-like content.
+    """
+
+    class Meta:
+        template = "home/applets/notes_applet.html"
+
+    title = CharBlock(max_length=20)
+    notes = ListBlock(
+        StructBlock(
+            [
+                ("title", CharBlock()),
+                (
+                    "content",
+                    RichTextBlock(
+                        features=[
+                            "h2",
+                            "bold",
+                            "italic",
+                            "link",
+                        ]
+                    ),
+                ),
+            ],
+            label="notes",
+        )
+    )
+
+
+class PomodoroApplet(StructBlock):
+    """
+    Applet to display a functional pomodoro.
+    """
+
+    class Meta:
+        template = "home/applets/pomodoro_applet.html"
+
+    title = CharBlock(max_length=20)
+
+
+class SocialLinksApplet(StructBlock):
+    """
+    Applet to display a list of links for social media like Instagram
+    or Twitter in a dropdown menu format.
+    """
+
+    class Meta:
+        template = "home/applets/social_links_applet.html"
+
+    title = CharBlock(max_length=20)
+    links = ListBlock(
+        StructBlock(
+            [
+                ("title", CharBlock()),
+                ("url", URLBlock()),
+            ],
+            label="links",
+        )
+    )
+
+
+class TestimoniesApplet(StructBlock):
+    """
+    Applet to display testimonies.
+    """
+
+    class Meta:
+        template = "home/applets/testimonies_applet.html"
+
+    title = CharBlock(max_length=20)
+    testimonies = ListBlock(
+        StructBlock(
+            [
+                ("quote", CharBlock()),
+                ("photo", DocumentChooserBlock()),
+                ("name", CharBlock()),
+                ("description", CharBlock()),
+            ],
+            label="testimonies",
+        )
+    )
+
+
+class TextualApplet(StructBlock):
+    """
+    Applet with a rich text body to display content as in a note-like app
+    format.
+    """
+
+    class Meta:
+        template = "home/applets/textual_applet.html"
+
+    title = CharBlock(max_length=20)
+    content = RichTextBlock(
+        features=[
+            "h2",
+            "bold",
+            "italic",
+            "link",
+        ]
+    )
+
+
+class TimelineApplet(StructBlock):
+    """
+    Applet to display a timeline.
+    """
+
+    class Meta:
+        template = "home/applets/timeline_applet.html"
+
+    title = CharBlock(max_length=20)
+    events = ListBlock(
+        StructBlock(
+            [
+                ("date", DateBlock()),
+                ("title", CharBlock(max_length=20)),
+                ("description", CharBlock()),
+            ],
+            label="events",
+        )
+    )
+
+
+class TodoApplet(StructBlock):
+    """
+    Applet to display a functional To Do list.
+    """
+
+    class Meta:
+        template = "home/applets/todo_applet.html"
+
+    title = CharBlock(max_length=20)
+    tasks = ListBlock(CharBlock(), label="tasks")
+
+
+class VideoApplet(StructBlock):
+    """
+    Applet to showcase videos.
+    """
+
+    class Meta:
+        template = "home/applets/video_applet.html"
+
+    title = CharBlock(max_length=20)
+    media = DocumentChooserBlock()
+
+
 class AppletsBlock(StreamBlock):
     """
     Block of applets.
     """
 
-    textual_applet = TextualApplet()
-    video_applet = VideoApplet()
-    social_links_applet = SocialLinksApplet()
-    todo_applet = TodoApplet()
-    notes_applet = NotesApplet()
     button_applet = ButtonApplet()
     contact_applet = ContactApplet()
+    notes_applet = NotesApplet()
+    pomodoro_applet = PomodoroApplet()
+    social_links_applet = SocialLinksApplet()
+    testimonies_applet = TestimoniesApplet()
+    textual_applet = TextualApplet()
+    timeline_applet = TimelineApplet()
+    todo_applet = TodoApplet()
+    video_applet = VideoApplet()
