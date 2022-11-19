8721ec80ae57d06e29dc1a7dbc066ac140eedcad
daniarlert <arlertdaniel@gmail.com>
2022-11-19 17:24:14 +0100

    feat: improve Dockerfile with setup script

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
+
+DJANGO_SUPERUSER_PASSWORD=$SUPER_USER_PASSWORD python manage.py createsuperuser --username $SUPER_USER_NAME --email $SUPER_USER_EMAIL --noinput
+
+gunicorn portfolio.wsgi:application --bind 0.0.0.0:8000
